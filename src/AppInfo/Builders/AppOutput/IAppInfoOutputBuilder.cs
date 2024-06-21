3d88e0788c594a07bfc9c0f933986f5567b0b103
Björn <bjorn@sentiandream.com>
2024-06-21 23:50:16 +0200

    refactor: move output to interfaces

--- src/AppInfo/Builders/AppOutput/IAppInfoOutputBuilder.cs
+++ src/AppInfo/Builders/AppOutput/IAppInfoOutputBuilder.cs
@@ -4,14 +4,30 @@ using AppInfo.Builders.AppInfo;
 namespace AppInfo.Builders.AppOutput;
 
 
-public interface IAppInfoOutputBuilder
+public interface IAppInfoOutputBuilder :
+	IAppOutputConsole,
+	IAppOutputTrace,
+	IAppOutputLog;
+
+
+public interface IAppOutputConsole
 {
 	IAppInfoOutputBuilder ToConsole();
-	IAppInfoOutputBuilder ToTrace();
 }
 
 
-public interface IAppOutput
+public interface IAppOutputTrace
+{
+	IAppInfoOutputBuilder ToTrace();
+}
+
+public interface IAppOutputLog
+{
+	IAppInfoOutputBuilder ToLog(Action<string> logger);
+}
+
+
+public interface IAppInfoOutput
 {
 	IAppInfoBuilder WithOutput(Action<IAppInfoOutputBuilder> configure);
 }
