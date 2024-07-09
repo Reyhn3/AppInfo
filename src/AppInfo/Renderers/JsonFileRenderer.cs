e15d269dfb1fdcb21e04c141161efa14006f5664
Björn <bjorn@sentiandream.com>
2024-07-10 00:35:42 +0200

    feat: json file rendering

--- src/AppInfo/Renderers/JsonFileRenderer.cs
+++ src/AppInfo/Renderers/JsonFileRenderer.cs
@@ -1,13 +1,36 @@
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
 namespace AppInfo.Renderers;
 
 
 public class JsonFileRenderer : Renderer
 {
+	private static readonly JsonSerializerOptions s_options = new()
+		{
+			WriteIndented = true,
+			Converters =
+				{
+					new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+				}
+		};
+
 	protected override void RenderAppInfo(IAppInfo info)
 	{
-//TODO: Format for JSON file
-		var formatted = string.Join(" ␍␊ ", info.Fragments.Select(f => string.Join('/', f.Value)));
-//TODO: Write to file
-		Console.WriteLine(formatted);
+		var container = BuildContainer(info);
+		var path = Path.ChangeExtension(Path.GetTempFileName(), "json");
+
+		using var stream = File.Create(path);
+		JsonSerializer.Serialize(stream, container, s_options);
+		Debug.WriteLine($"JSON file written to {path}", Constants.LibraryName);
+
+//TODO: Remove this
+		Console.WriteLine("JSON file: {0}", path);
 	}
+
+//TODO: Replace the object type with a structured type
+	private object BuildContainer(IAppInfo info) =>
+		info.Fragments.ToArray();
 }
