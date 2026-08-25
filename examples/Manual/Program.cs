69ebbec543929a2cf538b3e8245afcd83c109631
Björn <bjorn@sentiandream.com>
2026-08-25 14:42:12 +0200

    feat: add manual demo

---
+++ examples/Manual/Program.cs
@@ -0,0 +1,57 @@
+﻿// This demonstrates the manual and the fluent way to
+// configure and execute AppInfo.
+
+using System.Globalization;
+using System.Reflection;
+using AppInfo;
+using AppInfo.Extractors;
+using AppInfo.Renderers;
+
+
+//
+// MANUAL
+// This is the manual way to configure and execute AppInfo.
+// This clearly demonstrates that extracting information and
+// rendering are two separate concerns. Outputting the info
+// is optional.
+//
+
+// First, define what information to include
+var appInfoBuilder = new AppInfoBuilder();
+appInfoBuilder.UseCulture(CultureInfo.CreateSpecificCulture("sv-SE"));
+appInfoBuilder.AddExtractor(new StandardExtractor(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()));
+appInfoBuilder.AddExtractor<TimestampExtractor>();
+
+// Second, build the AppInfo
+var appInfo = appInfoBuilder.Build();
+
+// Third, declare what to do with the AppInfo
+var appInfoOutputBuilder = new AppInfoOutputBuilder();
+appInfoOutputBuilder.UseAppInfo(appInfo);
+appInfoOutputBuilder.AddRenderer<ConsoleRenderer>();
+
+// Fourth, execute the output
+appInfoOutputBuilder.Write();
+
+
+//
+//
+//
+Console.WriteLine("--------------------------------------------------");
+//
+//
+//
+
+
+//
+// FLUENT
+// This does the same way as the manual way,
+// but easier.
+//
+
+AppInfoBuilder
+	.CreateDefaultBuilder() // This adds the standard extractor
+	.UseCulture(CultureInfo.CreateSpecificCulture("sv-SE"))
+	.AddTimestamp()
+	.BuildAndWriteTo(output => output
+		.ToConsole());
