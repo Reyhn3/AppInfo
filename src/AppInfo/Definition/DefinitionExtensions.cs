5b7ff16740c2b63eccc99c7b9d0364608a52bad9
Björn <bjorn@sentiandream.com>
2026-08-30 15:22:54 +0200

    test: definition convenience members

--- src/AppInfo/Definition/DefinitionExtensions.cs
+++ src/AppInfo/Definition/DefinitionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Reflection;
 using AppInformation.Extractors;
 
@@ -30,8 +31,22 @@ public static class DefinitionExtensions
 			return appInfo;
 		}
 
+		private IAppInfoBuilder SafelyAddExtractor(Func<IExtractor> factory)
+		{
+			try
+			{
+				var extractor = factory();
+				return builder.AddExtractor(extractor);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Exception caught when trying to add extractor: {0}", ex);
+				return builder;
+			}
+		}
+
 		public IAppInfoBuilder AddStandard() =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new StandardExtractor(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()));
 
 		public IAppInfoBuilder WithIdentities(
@@ -39,7 +54,7 @@ public static class DefinitionExtensions
 			string? instanceId = null,
 			Func<object?>? scopeIdFactory = null,
 			params string[] args) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new IdentityExtractor(
 					appId,
 					instanceId,
@@ -48,36 +63,36 @@ public static class DefinitionExtensions
 					args));
 
 		public IAppInfoBuilder AddTimestamp() =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new TimestampExtractor());
 
 		public IAppInfoBuilder AddExtra(
 			string label,
 			object? value) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new ExtrasExtractor((label, value)));
 
 		public IAppInfoBuilder AddExtra(
 			string label,
 			Func<object?> valueFactory) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new ExtrasExtractor((label, valueFactory)));
 
 		public IAppInfoBuilder AddExtra(
 			params (string Label, object? Value)[] extras) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new ExtrasExtractor(extras));
 
 		public IAppInfoBuilder AddExtra(
 			params (string Label, Func<object?> ValueFactory)[] extras) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new ExtrasExtractor(extras));
 
 		public IAppInfoBuilder AddAssembly(
 			Assembly assembly,
 			string? shortName = null,
 			bool stripSourceRevision = false) =>
-			builder.AddExtractor(
+			builder.SafelyAddExtractor(() =>
 				new AssemblyExtractor(assembly, shortName, stripSourceRevision));
 	}
 }
--- tests/AppInfo.Tests/Definition/DefinitionExtensionsTests.cs
+++ tests/AppInfo.Tests/Definition/DefinitionExtensionsTests.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using AppInformation.Extractors;
 using AppInformation.Tests.TestHelpers;
 
 
@@ -6,6 +8,7 @@ namespace AppInformation.Tests.Definition;
 
 public class DefinitionExtensionsTests
 {
+	private const string ExtractorsFieldName = "_extractors";
 	private AppInfoBuilder _sut;
 
 	[SetUp]
@@ -21,7 +24,6 @@ public class DefinitionExtensionsTests
 		IAppInfo result;
 		string? output;
 
-
 		// Act
 
 		using (var capture = new StdOutCapture())
@@ -29,7 +31,6 @@ public class DefinitionExtensionsTests
 			result = _sut.BuildAndWriteToDefault();
 			output = capture.Captured.ToString();
 		}
-
 
 		// Assert
 
@@ -51,7 +52,6 @@ public class DefinitionExtensionsTests
 		IAppInfo result;
 		string? output;
 
-
 		// Act
 
 		using (var capture = new StdOutCapture())
@@ -61,7 +61,6 @@ public class DefinitionExtensionsTests
 			output = capture.Captured.ToString();
 		}
 
-
 		// Assert
 
 		result.ShouldNotBeNull();
@@ -69,6 +68,127 @@ public class DefinitionExtensionsTests
 		renderer.HasRenderBeenCalled.ShouldBeTrue();
 		renderer.AppInfo.ShouldNotBeNull();
 		output.ShouldBeEmpty("Did not expect to find any standard output");
+	}
+#endregion
+
+#region AddStandard
+	[Test]
+	public void AddStandard_shall_add_the_standard_extractor()
+	{
+		var result = _sut.AddStandard();
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is StandardExtractor);
+	}
+#endregion
+
+#region WithIdentities
+	[Test]
+	public void WithIdentities_shall_do_nothing_if_trying_to_add_the_identity_extractor_throws_exception()
+	{
+		var result = _sut.WithIdentities(null);
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldBeEmpty();
+	}
+
+	[Test]
+	public void WithIdentities_shall_add_the_identity_extractor()
+	{
+		var result = _sut.WithIdentities("test-app-id");
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is IdentityExtractor);
+	}
+#endregion
+
+#region AddTimestamp
+	[Test]
+	public void AddTimestamp_shall_add_the_timestamp_extractor()
+	{
+		var result = _sut.AddTimestamp();
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is TimestampExtractor);
+	}
+#endregion
+
+#region AddExtra
+	[Test]
+	public void AddExtra_with_single_KVP_shall_add_the_timestamp_extractor()
+	{
+		var result = _sut.AddExtra("test-label", "test-value");
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is ExtrasExtractor);
+	}
+
+	[Test]
+	public void AddExtra_with_single_value_factory_shall_add_the_timestamp_extractor()
+	{
+		var result = _sut.AddExtra("test-label", () => "test-value");
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is ExtrasExtractor);
+	}
+
+	[Test]
+	public void AddExtra_with_multiple_KVP_shall_add_the_timestamp_extractor()
+	{
+		var result = _sut.AddExtra(
+			("test-label-1", "test-value-1"),
+			("test-label-2", "test-value-2"));
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is ExtrasExtractor);
+	}
+
+	[Test]
+	public void AddExtra_with_multiple_value_factory_shall_add_the_timestamp_extractor()
+	{
+		var result = _sut.AddExtra(
+			("test-label-1", () => "test-value-1"),
+			("test-label-2", () => "test-value-2"));
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is ExtrasExtractor);
+	}
+#endregion
+
+#region AddAssembly
+	[Test]
+	public void AddAssembly_shall_add_the_assembly_extractor()
+	{
+		var result = _sut.AddAssembly(A.Dummy<Assembly>());
+		result.ShouldNotBeNull();
+
+		var extractors = TestHelpers.Helpers.GetFieldValue<List<IExtractor>>(_sut, ExtractorsFieldName);
+		extractors.ShouldNotBeNull();
+		extractors.ShouldNotBeEmpty();
+		extractors.ShouldContain(e => e is AssemblyExtractor);
 	}
 #endregion
 }
