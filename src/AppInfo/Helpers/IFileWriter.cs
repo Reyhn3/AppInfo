e19d937e77a35131349b9b168702ab3f9007a069
Björn <bjorn@sentiandream.com>
2026-08-31 22:16:43 +0200

    test: filewriter

---
+++ src/AppInfo/Helpers/IFileWriter.cs
@@ -0,0 +1,29 @@
+using System.Text;
+
+
+namespace AppInformation.Helpers;
+
+
+internal interface IFileWriter
+{
+	FileInfo? WriteToFile(string pathAndFileName, string? contents);
+}
+
+
+internal class FileWriter : IFileWriter
+{
+	public FileInfo? WriteToFile(string pathAndFileName, string? contents)
+	{
+		try
+		{
+			InternalLogger.Log("Writing to file '{0}'", pathAndFileName);
+			File.WriteAllText(pathAndFileName, contents, Encoding.UTF8);
+			return new FileInfo(pathAndFileName);
+		}
+		catch (Exception ex)
+		{
+			InternalLogger.Log("Exception caught when writing to file '{0}': {1}", pathAndFileName, ex);
+			return null;
+		}
+	}
+}
---
+++ tests/AppInfo.Tests/Helpers/FileWriterTests.cs
@@ -0,0 +1,31 @@
+using AppInformation.Helpers;
+
+
+namespace AppInformation.Tests.Helpers;
+
+
+public class FileWriterTests
+{
+	private FileWriter _sut;
+
+	[SetUp]
+	public void PreRun() =>
+		_sut = new FileWriter();
+
+	[Test]
+	public void WriteToFile_shall_not_throw_exception_on_errors() =>
+		Should.NotThrow(() => _sut.WriteToFile(Path.GetInvalidFileNameChars()[0].ToString(), "test"));
+
+	[Test]
+	public void WriteToFile_shall_write_the_content_to_the_specified_file()
+	{
+		var pathAndFileName = Path.GetTempFileName();
+		var result = _sut.WriteToFile(pathAndFileName, "test");
+
+		result.ShouldNotBeNull();
+		Console.WriteLine(result);
+
+		File.Exists(pathAndFileName).ShouldBeTrue();
+		File.ReadAllText(pathAndFileName).ShouldBe("test");
+	}
+}
